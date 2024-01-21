694f159e8f012f31eb5b55a1872a0eab137f4af1
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-21 17:19:37 +0330

    update: deploy scripts

--- teardown.sh
+++ teardown.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-helm uninstall ptaas
+docker compose down
--- tearup.sh
+++ tearup.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-helm install mysql charts/mysql
-
-helm install minio charts/minio
-
-helm install ptaas -f ptaas/values.versions.yaml ptaas
+docker compose up -d
